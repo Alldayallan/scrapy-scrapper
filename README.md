65bb74179a879c47b9d59381630550118d10ceb6
Alldayallan <54728152+Alldayallan@users.noreply.github.com>
2024-03-21 01:14:11 +0000

    project complete

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # scrapy-scrapper
-Week 3 scrapping project
+
+## Week 3 scrapping with scrapy project
+
+```
+$ python3 -m venv venv
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+```
+
+Use XPath and Scrapy to crawl a an html file generated from mitre.org
+
+```
+srapy crawl cve
+```
+
+and cve file was created for further assesment
+
+## README is a work in progress
--- vulnerabilities/vulnerabilities/spiders/cve.py
+++ vulnerabilities/vulnerabilities/spiders/cve.py
@@ -8,3 +8,26 @@ url = os.path.join(current_dir, "source-EXPLOIT-DB.html")
 class CveSpider(scrapy.Spider):
     name = "cve"
     allowed_domains = ["cve.mitre.org"]
+    start_urls = [f"file://{url}"]
+
+    def parse(self, response):
+        for child in response.xpath('//table'):
+            if len(child.xpath('tr')) > 100:
+                table = child
+                break
+        
+        count = 0
+        csv_file = open ('vulnerabilites.csv', 'w')
+        writer = csv.writer(csv_file)
+        writer.writerow(['exploit id', 'cve id'])
+        for row in table.xpath('//tr'):
+            if count > 100:
+                break
+            try:
+                exploit_id = row.xpath('td//text()')[0].extract()
+                cve_id = row.xpath('td//text()')[2].extract()
+                writer.writerow([exploit_id, cve_id])
+                count += 1
+            except IndexError:
+                pass
+            csv_file.close()
